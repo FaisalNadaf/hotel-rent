83b457c00c1c9d3d93e795ccab11df1ca6f355ae
faisal <faisalfnadaf@gmail.com>
2024-01-23 16:30:07 +0530

    code

--- app.js
+++ app.js
@@ -1,98 +1,114 @@
-const express=require("express");
-const app=express();
-const mongoose=require("mongoose");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 const listing = require("./models/listing.js");
 const path = require("path");
 const Listing = require("./models/listing.js");
-const methodOverride =require("method-override");
-const  ejsMate= require('ejs-mate');
-const wrapAsync=require("./utility/wrapAsync.js");
-const ExpressError=require("./utility/expressError.js");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utility/wrapAsync.js");
+const ExpressError = require("./utility/expressError.js");
 
-app.set("View engine","ejs");
-app.set("View",path.join(__dirname,"Views"));
-app.use(express.static(path.join(__dirname,"/public")));
-app.use(express.urlencoded({extended:true}));
+app.set("View engine", "ejs");
+app.set("View", path.join(__dirname, "Views"));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs',ejsMate);
+app.engine("ejs", ejsMate);
 
-let mongo_url="mongodb://127.0.0.1:27017/wonderlust";
+let mongo_url = "mongodb://127.0.0.1:27017/wonderlust";
 /*---------------------------------------main---------------------------------------------*/
-main().then(()=>{
+main()
+  .then(() => {
     console.log("mongodb connected");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
-async function main(){
-    await mongoose.connect(mongo_url);
+async function main() {
+  await mongoose.connect(mongo_url);
 }
 
-
 /*---------------------------------------root route---------------------------------------------*/
-app.get("/",(req,res)=>{
-    res.send("hello im root");
+app.get("/", (req, res) => {
+  res.send("hello im root");
 });
 
 /*---------------------------------------index route---------------------------------------------*/
-app.get("/listings",async(req,res)=>{
-    const allListing= await listing.find({});
-    res.render("listings/index.ejs",{allListing});
-
-});
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    const allListing = await listing.find({});
+    res.render("listings/index.ejs", { allListing });
+  })
+);
 
 /*---------------------------------------new route---------------------------------------------*/
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs");
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
 });
 /*---------------------------------------show route---------------------------------------------*/
- app.get("/listings/:id", async(req,res)=>{
-    let {id}=req.params;
-    const listing =await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
- }); 
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+  })
+);
 
 /*---------------------------------------create new listing route---------------------------------------------*/
-app.post("/listings",wrapAsync(async(req,res,next)=>{
-        const newListing = new listing(req.body.listing);
-        await newListing.save();
-         res.redirect("/listings");
-       
- }));
+app.post(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
+    const newListing = new listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
 
- /*---------------------------------------edit listing route---------------------------------------------*/
- app.get("/listings/:id/edit", async(req,res)=>{
-    let {id}=req.params;
-    const listing =await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
- }); 
+/*---------------------------------------edit listing route---------------------------------------------*/
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
 
 /*---------------------------------------update listing route---------------------------------------------*/
-app.put("/listings/:id", async(req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+app.put(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
- }); 
+  })
+);
 
- /*---------------------------------------delete listing route---------------------------------------------*/
-app.delete("/listings/:id", async(req,res)=>{
-    let {id}=req.params;
+/*---------------------------------------delete listing route---------------------------------------------*/
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
- }); 
+  })
+);
 
- /*---------------------------------------ERROR HANDLING---------------------------------------------*/
-app.all("*",(req,res,next)=>{
-next(ExpressError(404,"page not found !"));
+/*---------------------------------------ERROR HANDLING---------------------------------------------*/
+app.all("*", (req, res, next) => {
+  next(ExpressError(404, "page not found !"));
 });
-app.use((err,req,res,next)=>{
-    let{statusCode,message}=ree;
-res.send(statusCode).send(message);
+app.use((err, req, res, next) => {
+  let { statusCode, message } = err;
+  res.send(statusCode).send(message);
 });
-
 
 /*---------------------------------------port---------------------------------------------*/
-app.listen(8080,() =>{
+app.listen(8080, () => {
   console.log("server started on port 8080");
 });
