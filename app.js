d4a6e3cbe809166f61c1e94b8086b5b2d0db15d7
faisal <faisalfnadaf@gmail.com>
2024-01-27 15:29:01 +0530

    code

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const listing = require("./models/listing.js");
 const path = require("path");
-const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utility/wrapAsync.js");
 const ExpressError = require("./utility/expressError.js");
-const { listingSchema,reviewSchema } = require("./schema.js");
-const review = require("./models/review.js");
 const listings=require("./routes/listing.js");
 const review=require("./routes/review.js");
-
-const { log } = require("console");
-
+/*------------------------------------------------------------------------------------*/
 app.set("View engine", "ejs");
 app.set("View", path.join(__dirname, "Views"));
 app.use(express.static(path.join(__dirname, "/public")));
@@ -22,8 +16,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use("/listings",listings);
-app.use("/reviews",review);
-
+app.use("/listing/:id/reviews",review);
+/*------------------------------------------------------------------------------------*/
 let mongo_url = "mongodb://127.0.0.1:27017/wonderlust";
 /*---------------------------------------main---------------------------------------------*/
 main()
@@ -37,14 +31,10 @@ main()
 async function main() {
   await mongoose.connect(mongo_url);
 }
-
-
-
 /*---------------------------------------root route---------------------------------------------*/
 app.get("/", (req, res) => {
   res.send("hello im root");
 });
-
 /*---------------------------------------ERROR HANDLING---------------------------------------------*/
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "page not found!"));
@@ -54,7 +44,6 @@ app.use((err, req, res, next) => {
   let { statusCode = 500, message = "somthing went wrong" } = err;
   res.render("./listings/error.ejs", { message });
 });
-
 /*---------------------------------------port---------------------------------------------*/
 app.listen(8080, () => {
   console.log("server started on port 8080");
--- routes/review.js
+++ routes/review.js
@@ -1,11 +1,12 @@
 
 const express = require("express");
-const router = express.Router();
+const router = express.Router({mergeParams:true});
 const wrapAsync = require("../utility/wrapAsync.js");
 const ExpressError = require("../utility/expressError.js");
-const { listingSchema,reviewSchema } = require("../schema.js");
+const { reviewSchema } = require("../schema.js");
 const review = require("../models/review.js");
-
+const listing = require("../models/listing.js");
+const Listing = require("../models/listing.js");
 
 /*---------------------------------------validatereview---------------------------------------------*/
 const validateReview = (req, res, next) => {
@@ -20,7 +21,7 @@ const validateReview = (req, res, next) => {
 
   
 /*---------------------------------------reviwe post route---------------------------------------------*/
-router.post("/listings/:id/reviews",validateReview,wrapAsync( async (req, res) => {
+router.post("/",validateReview,wrapAsync( async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = await new review(req.body.review);
   
@@ -32,7 +33,7 @@ router.post("/listings/:id/reviews",validateReview,wrapAsync( async (req, res) =
   }));
   
   /*---------------------------------------reviwe delete route---------------------------------------------*/
-  router.delete("/listins/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+  router.delete("/:reviewId",wrapAsync(async(req,res)=>{
     let {id,reviewId}=req.params;
     await listing.findByIdAndUpdate(id,({$pull:{review:reviewId}}));
   await review.findByIdAndDelete("reviewId");
